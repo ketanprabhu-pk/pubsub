ece1ef55bb5f648c1ad7e9c9ae6e281f8b373f9f
ketanprabhu-pk <ketanprabhu1297@gmail.com>
2023-01-27 00:17:34 +0530

    Innitial

---
+++ src/test/java/pk/ketanprabhu/pubsub/PubsubApplicationTests.java
@@ -0,0 +1,13 @@
+package pk.ketanprabhu.pubsub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PubsubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
